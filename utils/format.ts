fc262b628525bae29b3eb280237424c70d74c3a0
Yshay Yaacobi <yshay@yshay-mac.soluto.local>
2020-06-07 16:47:51 +0300

    fix

--- utils/format.ts
+++ utils/format.ts
@@ -2,7 +2,6 @@ import { stringify } from "https://deno.land/std/encoding/yaml.ts";
 import { cleanObject } from "./object.ts";
 
 export function formatYaml(o: any) {
-  console.log(o);
   return Object.values(o as { [key: string]: any }).map((x) => {
     cleanObject(x);
     return stringify(x);
