4799bc253c06fe7ce8e36036e489d3bc831d5bf9
Yshay Yaacobi <yshay@yshay-mac.soluto.local>
2020-06-07 14:47:41 +0300

    fix format to drop undefined

--- utils/format.ts
+++ utils/format.ts
@@ -1,6 +1,27 @@
 import { stringify } from "https://deno.land/std/encoding/yaml.ts";
 
+function cleanObject(x: any) {
+  if (typeof (x) === "object") {
+    if (Array.isArray(x)) {
+      x.forEach(cleanObject);
+    } else {
+      Object.keys(x).forEach((key) => {
+        if (x[key] === undefined) {
+          delete x[key];
+        } else {
+          cleanObject(x[key]);
+        }
+      });
+      x;
+    }
+  }
+}
+
 export function formatYaml(o: any) {
-  return Object.values(o as { [key: string]: any }).map((x) => stringify(x))
+  console.log(o);
+  return Object.values(o as { [key: string]: any }).map((x) => {
+    cleanObject(x);
+    return stringify(x);
+  })
     .join("---\n");
 }
