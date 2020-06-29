6417545c51f2d5a068ecaab53698483f3386315e
Yshay Yaacobi <yshayy@gmail.com>
2020-06-29 16:10:03 +0000

    update version

--- examples/01.basic-k8s-blueprint/deploy.ts
+++ examples/01.basic-k8s-blueprint/deploy.ts
@@ -1,12 +1,12 @@
-import { createKubeBlueprint } from "https://deno.land/x/gh:yshayy:deploykit@0.0.16/blueprint/k8s/mod.ts";
+import { createKubeBlueprint } from "https://deno.land/x/gh:yshayy:deploykit@0.0.17/blueprint/k8s/mod.ts";
 import {
   addDeployment,
   addService,
   expose,
   addResource,
 } from "../../blueprint/k8s/operators/all.ts";
-import * as crds from "https://deno.land/x/gh:yshayy:deploykit@0.0.16/generated/k8s/crds/mod.ts";
-import { KubeMetaContext } from "https://deno.land/x/gh:yshayy:deploykit@0.0.16/blueprint/k8s/types.ts";
+import * as crds from "https://deno.land/x/gh:yshayy:deploykit@0.0.17/generated/k8s/crds/mod.ts";
+import { KubeMetaContext } from "https://deno.land/x/gh:yshayy:deploykit@0.0.17/blueprint/k8s/types.ts";
 
 interface MyServiceParams extends KubeMetaContext {
   image: {
