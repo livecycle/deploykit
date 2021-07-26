317047435ffe2835d141a58f22ecce659c48fc2b
Yshay <yshay@livecycle.io>
2021-07-26 22:07:10 +0000

    small fix in format

--- utils/format.ts
+++ utils/format.ts
@@ -1,10 +1,10 @@
-import { stringify } from "https://deno.land/std/encoding/yaml.ts";
+import { stringify } from "https://deno.land/std@0.103.0/encoding/yaml.ts";
 import { cleanObject } from "./object.ts";
 
 export function formatYaml(o: any) {
   return Object.values(o as { [key: string]: any }).map((x) => {
     cleanObject(x);
-    return stringify(x);
+    return stringify(x, { noRefs: true });
   })
     .join("---\n");
 }
